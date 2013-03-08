b31639e27aa71b1549194f2c7633ea2249d03f4f
Matheus Lima <matheuslimaufc@gmail.com>
2013-03-07 23:17:12 -0300

    Implementada funcao para calcular o transposto de uma matriz

--- matrizes.c
+++ matrizes.c
@@ -46,5 +46,41 @@ tratarErro:
         free(matrizProduto);
         matrizProduto = NULL;
     }
+    goto retorno;
+}
 
+double** transposto(double **matriz, int linhas, int colunas)
+{
+    double** matrizTransposta = NULL;
+    int i,j;
+
+    matrizTransposta = (double**) malloc(colunas*sizeof(double*));
+    if(matrizTransposta == NULL) goto tratarErro;
+    for(i = 0; i < colunas; i++)
+    {
+        matrizTransposta[i] = (double*) malloc(linhas*sizeof(double));
+        if(matrizTransposta[i] == NULL) goto tratarErro;
+    }
+
+    for(i = 0; i < colunas; i++)
+        for(j = 0; j < linhas; j++)
+            matrizTransposta[i][j] = matriz[j][i];
+
+retorno:
+    return matrizTransposta;
+tratarErro:
+    if(matrizTransposta != NULL)
+    {
+        for(i = 0; i < colunas; i++)
+        {
+            if(matrizTransposta[i] != NULL)
+            {
+                free(matrizTransposta[i]);
+                matrizTransposta[i] = NULL;
+            }
+        }
+        free(matrizTransposta);
+        matrizTransposta = NULL;
+    }
+    goto retorno;
 }
--- matrizes.h
+++ matrizes.h
@@ -2,10 +2,15 @@
 #define MATRIZES_H
 
 // Implementacao de funcoes gerais para manipulacao de matrizes
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 // Efetua o produto entre duas matrizes A e B, desde que suas dimensoes
 // sejam MxN e NxP respectivamente.
-double **produto(double** matrizA, int linhasA, int colunasA, double** matrizB, int linhasB, int colunasB);
+double** produto(double** matrizA, int linhasA, int colunasA, double** matrizB, int linhasB, int colunasB);
+
+// Obtem o transposto de uma matriz
+double** transposto(double** matriz, int linhas, int colunas);
 
 #endif
