0373d6f7aa3f6fa4726f7c151735eb9107403297
Matheus Lima <matheuslimaufc@gmail.com>
2013-03-07 21:19:04 -0300

    Adicionado header de utilitarios. Criado enum e macro para manipulacao de status

---
+++ util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+// Enum para representar codigos de erro
+typedef enum
+{
+    sucesso = 0,
+    erroAlocacao = 1
+}Status;
+
+// Macro que verifica se um status eh sucesso
+#define OK(status)(*status == sucesso)
+
+#endif
