31cfe1008db76901500bf3e0e0c17db594c06db9
Matheus Lima <matheuslimaufc@gmail.com>
2013-11-24 15:26:31 -0300

    adicionado teste de calculo de determinante usando Gauss-Jordan

--- main.c
+++ main.c
@@ -1,34 +1,32 @@
-#include <stdio.h>
-#include "fatoracaoLU.h"
-#include "matriz.h"
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include"gaussJordan.h"
+#include"matriz.h"
 void imprimirMatriz(Matriz matriz);
+
+#define MATRIX_FILE "matrix.dat"
+#define LENGTH_LINE 100*20
+
+double **loadMatrix();
 
 int main(void)
 {
-    double matriz[3][3] = {{2,1,0},{1,2,1},{0,1,2}};
+    double **matrix = loadMatrix();
+    int i, j;
+    struct timespec start, stop;
+    if(matrix == NULL) exit(1);
     Matriz *pMatriz = NULL;
-    Matriz MatrizL ;
-    Matriz MatrizU;
-    int i,j;
-
-    pMatriz = criarMatriz(3,3);
-    if(pMatriz == NULL) return -1;
-
-    for(i = 0; i < 3; i++)
-        for(j = 0; j < 3; j++)
-            pMatriz->conteudo[i][j] = matriz[i][j];
-    pMatriz->linhas = 3;
-    pMatriz->colunas = 3;
-
-    printf("Matriz original:\n");
-    imprimirMatriz(*pMatriz);
-
-    fatorarMatriz(pMatriz, &MatrizL, &MatrizU);
-    printf("Matriz L:\n");
-    imprimirMatriz(MatrizL);
-    printf("Matriz U:\n");
-    imprimirMatriz(MatrizU);
-
+    pMatriz = criarMatriz(100, 100);
+    for(i = 0; i < 100; i++)
+        for(j = 0; j < 100; j++)
+            pMatriz->conteudo[i][j] = matrix[i][j];
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    double det = determinant(pMatriz);
+    clock_gettime(CLOCK_MONOTONIC, &stop);
+    printf("Determinante: %f\n", det);
+    printf("Tempo de calculo: %ld ns\n", ((stop.tv_sec * 1000000000) + stop.tv_nsec) -
+           ((start.tv_sec * 1000000000) + start.tv_nsec));
     return 0;
 }
 
@@ -41,4 +39,42 @@ void imprimirMatriz(Matriz matriz)
             printf("%f\t", matriz.conteudo[i][j]);
         printf("\n");
     }
+}
+
+double **loadMatrix(){
+    char line[LENGTH_LINE];
+    char c;
+    char *buffer = NULL;
+    int i, j;
+    if(line == NULL) return NULL;
+    double **matrix = NULL;
+    matrix = (double **)malloc(100*sizeof(double*));
+    if (matrix == NULL) return NULL;
+    for(i = 0; i < 100; i++){
+        matrix[i] = (double*)malloc(100*sizeof(double));
+        if(matrix[i] == NULL) return NULL;
+    }
+
+    FILE *pFile = NULL;
+    pFile = fopen(MATRIX_FILE, "r");
+
+    for(i = 0; i < 100; i++)
+    {
+        memset(line, '\0', LENGTH_LINE);
+        j = 0;
+        do{
+            c = getc(pFile);
+            line[j++] = c;
+        }while(c != '\n' && c != EOF);
+        j = 0;
+        buffer = strtok(line, " ");
+        matrix[i][j] = atof(buffer);
+        while(j < 99){
+            j++;
+            buffer = strtok(NULL, " ");
+            matrix[i][j] = atof(buffer);
+        }
+    }
+    fclose(pFile);
+    return matrix;
 }
