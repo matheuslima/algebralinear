8e00a5795927c5b5ddaac076a3f2fe9c870e2a94
Matheus Lima <matheuslimaufc@gmail.com>
2014-02-23 13:50:47 -0300

    corrigida declaracao da funcao subtrairVetores

--- gramSchmidt.h
+++ gramSchmidt.h
@@ -3,10 +3,10 @@
 
 double *obterVetorUnitario(double *vetor, int tamanho);
 
-double *subtrairVetores(double *vetorA, double *vetorB);
+double *subtrairVetores(double *vetorA, double *vetorB, int tamanho);
 
 // Projeta o vetor A sobre o vetor B
-double *obterProjecao(double *vetorA, *double vetorB, int tamanho);
+double *obterProjecao(double *vetorA, double*vetorB, int tamanho);
 
 // Obtem um espaco ortonormal a partir do algoritmo de ortogonalizacao de Gram-Schmidt
 double **obterEspacoOrtonormal(double **vetores, int quantidade, int tamanho);
